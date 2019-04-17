54b10937853bfcd699b4bdc08121741b5929e18c
Ethan Wright <ethan.wright@charter.com>
2019-04-16 23:30:34 -0600

    Moving plugins to json file

--- bin/install.py
+++ bin/install.py
@@ -1,4 +1,5 @@
 from git import Repo
+import json
 import distutils
 from colorama import init
 from termcolor import colored
@@ -8,129 +9,8 @@ from sys import platform
 from os.path import expanduser
 import os
 
-config = {
-    "plugins":
-    [{
-        "name": "Polygot",
-        "description": "Better Syntax Highlighting.",
-        "git": ["https://github.com/sheerun/vim-polyglot"],
-        "use": ["Disable languages with: let g:polyglot_disabled = ['css']"]
-    },
-        {
-        "name": "Minimalist",
-        "description": "A material color scheme for darker vim.",
-        "git": ["https://github.com/dikiaap/minimalist"],
-        "use": ["In vimrc: colorscheme minimalist"]
-    },
-        {
-        "name": "Monokai Tasty",
-        "description": "Sublime Monokai inspired color scheme.",
-        "git": ["https://github.com/patstockwell/vim-monokai-tasty.git"],
-        "use": ["In vimrc: colorscheme vim-monokai-tasty"]
-    },
-        {
-        "name": "FZF",
-        "description": "Fzf File Finder",
-        "git": ["https://github.com/junegunn/fzf.git",
-                "https://github.com/junegunn/fzf.vim.git"],
-        "use": ["ctrl-p to open",
-                "ctrl-x horizontal split",
-                "ctrl-v vertical split",
-                "ctrl-t new tab"]
-    },
-        {
-        "name": "Lightline",
-        "description": "A light and configurable \
-         statusline/tabline plugin for Vim",
-        "git": ["https://github.com/itchyny/lightline.vim"],
-        "use": ['''let g:lightline ={
-                       \ 'colorscheme': 'wombat',
-                       \ 
-                 }''']
-    },
-        {
-        "name": "Tagbar",
-        "description": "Displays tags in a window, ordered by scope",
-        "git": ["https://github.com/majutsushi/tagbar.git"],
-        "use": ["ctrl-t to open",
-                ":T to open, if open jumps to window"]
-    },
-        {
-        "name": "Auto Pairs",
-        "description": "Insert or delete brackets, parens, quotes in pair",
-        "git": ["https://github.com/jiangmiao/auto-pairs.git"],
-        "use": ["Automatic when installed."]
-    },
-        {
-        "name": "Surround",
-        "description": "Quoting/parenthesizing made simple.",
-        "git": ["https://github.com/tpope/vim-surround.git"],
-        "use": ["cs\"' | \"Hello World\" changes to 'Hello World'",
-                "cs'<q> | 'Hello World' changes to <q>Hello World</q>",
-                "ds' 'Hello World' changes to Hello World"]
-    },
-        {
-        "name": "Repeat",
-        "description": "Enable repeating supported plugin maps with '.'",
-        "git": ["https://github.com/tpope/vim-repeat.git"],
-        "use": ["Allows plugins like surround to work with . operator"]
-    },
-        {
-        "name": "Peekaboo",
-        "description": "Extends \" and @ in normal mode and <CTRL-R> in \
-                    insert mode so you can \
-                    see the coontents of the registers.",
-        "git": ["https://github.com/junegunn/vim-peekaboo.git"],
-        "use": ["press \" or @ for a sidebar to pop up, showing you the \
-                 contents of your registers.",
-                "\"add | delete and copy current line into register a.",
-                "\"ap | paste contents of register a to current line"]
-    },
-        {
-        "name": "Easy Motion",
-        "description": "Vim motions on speed!",
-        "git": ["https://github.com/easymotion/vim-easymotion.git"],
-        "use": ["shift-j in normal mode | shows characters for jumping below \
-                 cursor.",
-                "shift-k in normal mode | show characters for jumping above \
-                 cursor."]
-    },
-        {
-        "name": "Matchit",
-        "description": "Extends % matching for HTML, LaTeX, and many \
-                        other languages.",
-        "git": ["https://github.com/tmhedberg/matchit.git"],
-        "use": ["allows % to jump from more robust set of tags"]
-    },
-        {
-        "name": "ZoomWin",
-        "description": "Zoom in/out of windows \
-                    (toggle between one window and multi-window)",
-        "git": ["https://github.com/vim-scripts/ZoomWin.git"],
-        "use": ["<CTRL>-O zooms into current VIM pane or zoom out of it.",
-                "CAUTION: Some issues arise when TagBar is open."]
-    },
-        {
-        "name": "Goyo",
-        "description": "Distraction-free writing in VIM.",
-        "git": ["https://github.com/junegunn/goyo.vim.git"],
-        "use": [":G | toggle Goyo on or off.",
-                "let g:goyo_width=100"]
-    },
-        {
-        "name": "WindowSwap",
-        "description": "Swap your windows without ruining your layout.",
-        "git": ["https://github.com/wesQ3/vim-windowswap.git"],
-        "use": ["Go to pane to move, press <leader>ww, go to destination, press <leader>ww"]
-    }],
-    "extra_plugins": [{
-        "name": "Ale Linter",
-        "description": "Asynchronous linting/fixing for Vim and Language Server Protocol (LSP) integration.",
-        "git": ["https://github.com/w0rp/ale.git"],
-        "use": [":ALEFix | Run through file and attempt to fix mistakes.",
-                ":ALEToggle | Turn ALE on or off for the file."]
-    }]
-}
+with open('./plugins.json') as json_file:
+    config = json.load(json_file)
 
 
 cp_files = {
@@ -143,8 +23,7 @@ cp_files = {
     "../conf/autoload/*": "~/.vim/autoload/",
     "../conf/skeletons": "~/.vim",
     "../conf/vimrc": "~/.vimrc",
-    "../conf/linters/.*": "~/",
-    "../conf/linters/.config/*": "~/.config/",
+    "../conf/linters/*": "~",
     "../conf/fzf_functions": "~/.fzf_functions"
 }
 
@@ -152,7 +31,7 @@ init()
 
 
 def cp(src, dst):
-    os.system("cp -r " + src + " " + dst)
+    os.system(add_user("cp -r " + src + " " + dst))
 
 
 def add_user(file):
