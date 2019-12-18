26ca7a8dbbbe61b2f017f7cd529b3bd0e0e4404f
Ethan Wright <ethan.wright@charter.com>
2019-12-17 20:41:28 -0700

    Adding plug plugin manager

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ sudo add-apt-repository ppa:deadsnakes/ppa
 sudo add-apt-repository ppa:jonathonf/vim
 sudo apt-get update
 sudo apt-get install -y python3.6 vim
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
